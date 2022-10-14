e3120ea9308d5dabf993519c210059eaf45eaea9
Andrew <andrewg87@gmail.com>
2022-10-14 11:00:03 -0400

    added arrays that contain the data that will be used for the password generator

--- script.js
+++ script.js
@@ -12,3 +12,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowerCaseLetters = ["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"]
+var specialCharacters = ["!", "\"", "#", "$", "%", "&", "\'", ")", "(", "*", ",", ".", "-","/", ":", ";", "<", ">", "=", "?", "[", "]", "~", "_", "{", "}", "^"]
